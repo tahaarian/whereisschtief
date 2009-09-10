0419f4d6b25f6d32c3612c8af48638e6e6804497
lischke <lischke@58813d38-6d59-11de-9c28-0bda66f61e62>
2009-09-10 16:11:00 +0000

    added new features

--- trunk/WhereIsSchtief/src/org/schtief/whereisschtief/WhereIsSchtiefServlet.java
+++ trunk/WhereIsSchtief/src/org/schtief/whereisschtief/WhereIsSchtiefServlet.java
@@ -24,8 +24,13 @@ public class WhereIsSchtiefServlet extends HttpServlet {
 	private static final String PARAMETER_ACTION	=	"action";
 	private static final String ACTION_ADD_USER		=	"adduser";
 	private static final String ACTION_GET_DATA		=	"getdata";
+	private static final String ACTION_GET_CLUSTER	=	"getcluster";
+	private static final String ACTION_EXPORT		=	"export";
+
 	private static final String PARAMETER_NAME 		= 	"name";
 	private static final String PARAMETER_DATE 		= 	"date";
+	private static final String PARAMETER_RADIUS 	= 	"radius";
+	private static final String PARAMETER_THRESHOLD	= 	"threshold";
 	private static final String PARAMETER_CALLBACK	= 	"callback";
 	private static final String PARAMETER_JSON_URL 	= 	"jsonurl";
 	
@@ -47,6 +52,16 @@ public class WhereIsSchtiefServlet extends HttpServlet {
 		else if(ACTION_GET_DATA.equals(action))
 		{
 			getData(req,resp);
+		}
+		//get cluster
+		else if(ACTION_GET_CLUSTER.equals(action))
+		{
+			getCluster(req,resp);
+		}
+		//export
+		else if(ACTION_EXPORT.equals(action))
+		{
+			export(req,resp);
 		}
 		else
 			error(resp,"Wrong action!");
@@ -164,6 +179,116 @@ public class WhereIsSchtiefServlet extends HttpServlet {
 		//return ok
 		return;
 	}
+	
+	private void getCluster(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	{
+		//optional name
+		String name		=	req.getParameter(PARAMETER_NAME);
+		String radius	=	req.getParameter(PARAMETER_RADIUS);
+		String threshold=	req.getParameter(PARAMETER_THRESHOLD);
+		
+		//callback
+		String callback	=	req.getParameter(PARAMETER_CALLBACK);
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+
+		String dateS	=	req.getParameter(PARAMETER_DATE);
+
+		Calendar startCal=Calendar.getInstance();
+		//By Default -1 Day
+		startCal.add(Calendar.MONTH, -1);
+		Calendar endCal=Calendar.getInstance();
+		if(null!=dateS)
+		{
+			try
+			{
+				//TODO try catch logging
+				//parse to calendar
+				DateFormat formatter = new SimpleDateFormat("dd.MM.yy");
+				Date     date = (Date)formatter.parse(dateS); 
+				startCal.setTime(date);
+				endCal.setTime(date);
+			}
+			catch(Exception e)
+			{
+				throw new RuntimeException("Parse Date error "+dateS,e);
+			}
+		}
+		// 00:00
+		startCal.set(Calendar.HOUR, 0);
+		startCal.set(Calendar.MINUTE, 0);
+		startCal.set(Calendar.SECOND, 0);
+		
+		//By Default 23:59
+		endCal.set(Calendar.HOUR, 23);
+		endCal.set(Calendar.MINUTE, 59);
+		endCal.set(Calendar.SECOND, 59);
+		
+		List<Location> locations	=	LocationManager.getLocations(pm,name,startCal,endCal);
+		
+		//build cluster
+		//punkte mit längster dauer suchen
+		//write javascript+json
+		resp.setContentType("text/javascript");
+		resp.getWriter().append(callback+"(");
+		JSONWriter writer = new JSONWriter(resp.getWriter());
+		
+		try
+		{
+			writer.object();
+			writer.key("locations");
+			writer.array();
+			for (Location location : locations) {
+				writer.object();
+				location.toJSON(writer);
+				writer.endObject();
+			}
+			writer.endArray();
+			writer.endObject();
+			resp.getWriter().append(");");
+		}
+		catch(JSONException je)
+		{
+			je.printStackTrace();
+			error(resp,"could not write JSON");
+		}
+	}
+	
+	private void export(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	{
+		//optional name
+		String name	=	req.getParameter(PARAMETER_NAME);
+	
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+
+		List<Location> locations	=	LocationManager.getLocations(pm,name,startCal,endCal);
+		//write javascript+json
+		resp.setContentType("text/javascript");
+		resp.getWriter().append(callback+"(");
+		JSONWriter writer = new JSONWriter(resp.getWriter());
+		
+		try
+		{
+			writer.object();
+			writer.key("locations");
+			writer.array();
+			for (Location location : locations) {
+				writer.object();
+				location.toJSON(writer);
+				writer.endObject();
+			}
+			writer.endArray();
+			writer.endObject();
+			resp.getWriter().append(");");
+		}
+		catch(JSONException je)
+		{
+			je.printStackTrace();
+			error(resp,"could not write JSON");
+		}
+		
+	}
+	
 	private void error(ServletResponse resp, String message) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println(message);
