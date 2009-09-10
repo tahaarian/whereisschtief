3ddcf22eb568e244803173009f4c6fcbaad71185
lischke <lischke@58813d38-6d59-11de-9c28-0bda66f61e62>
2009-09-10 16:11:00 +0000

    added new features

--- WhereIsSchtief/src/org/schtief/whereisschtief/LocationManager.java
+++ WhereIsSchtief/src/org/schtief/whereisschtief/LocationManager.java
@@ -11,16 +11,26 @@ public class LocationManager
 	@SuppressWarnings("unchecked")
 	public static List<Location> getLocations(PersistenceManager pm, String name, Calendar fromDate, Calendar toDate)
 	{
+		StringBuffer jdoql	=	new StringBuffer("SELECT FROM ");
+		jdoql.append(Location.class.getName());
+		if(null!=fromDate || null!= toDate)
+		{			
+			jdoql.append(" WHERE");
+			if(null!=fromDate)
+			{
+				jdoql.append(" time >= ");
+				jdoql.append(fromDate.getTimeInMillis());
+			}
+			if(null!=toDate)
+			{
+				jdoql.append(" && time <= ");
+				jdoql.append(toDate.getTimeInMillis());
+			}
+		}
+		//TODO name
+		jdoql.append(" ORDER BY time DESC");
 
-		long startTimeStamp	=	fromDate.getTimeInMillis();
-		
-//		date.set(Calendar.HOUR, 23);
-//		date.set(Calendar.MINUTE, 59);
-//		date.set(Calendar.SECOND, 59);
-		long endTimeStamp	=	toDate.getTimeInMillis();
-		
-
-		String jdoql = "SELECT FROM " + Location.class.getName()+" WHERE time >= "+startTimeStamp+" && time <= "+endTimeStamp+" ORDER BY time DESC";
+//		String jdoql = "SELECT FROM " + Location.class.getName()+" WHERE time >= "+startTimeStamp+" && time <= "+endTimeStamp+" ORDER BY time DESC";
 		System.out.println("HALLO"+jdoql);
 		return (List<Location>) pm.newQuery(jdoql).execute();
 	}
