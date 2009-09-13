12d4c38e98bcae5cbdfa3981a1cd4924f5d15311
lischke <lischke@58813d38-6d59-11de-9c28-0bda66f61e62>
2009-09-13 00:36:20 +0000

    added clustering algorithm

--- trunk/WhereIsSchtief/src/org/schtief/whereisschtief/Location.java
+++ trunk/WhereIsSchtief/src/org/schtief/whereisschtief/Location.java
@@ -22,15 +22,22 @@ public class Location {
 	
 	@Persistent
 	private String latitude;
-
+	
+	private Double dLatitude;
+	
 	@Persistent
 	private String longitude;
+
+	private Double dLongitude;
 
 	public Location( Long time, String latitude, String longitude) {
 		super();
 		this.time = time;
 		this.latitude = latitude;
 		this.longitude = longitude;
+	}
+	
+	protected Location() {
 	}
 
 	public Long getId() {
@@ -48,10 +55,34 @@ public class Location {
 	public String getLongitude() {
 		return longitude;
 	}
+	
+	public Double getDoubleLongitude() {
+		if(null==dLongitude)
+		{
+			try{
+				dLongitude = Double.parseDouble(longitude);
+			}catch(NumberFormatException nfe){};
+		}
+		return dLongitude;
+	}
+
+	public Double getDoubleLatitude() {
+		if(null==dLatitude)
+		{
+			try{
+				dLatitude = Double.parseDouble(latitude);
+			}catch(NumberFormatException nfe){};
+		}
+		return dLatitude;
+	}
+
 
 	@Override
 	public String toString() {
 		return "Id: "+getId()+", Time: "+getTime()+", Lat: "+latitude+", Long: "+longitude;
+	}
+	public String toCSV() {
+		return getId()+","+getTime()+","+latitude+","+longitude;
 	}
 
 	@SuppressWarnings("deprecation")
@@ -63,11 +94,54 @@ public class Location {
 		cal.setTimeInMillis(time);
 		cal.add(Calendar.HOUR, 2);
 		writer.value(cal.getTime().toGMTString());
+		
 		writer.key("latitude");
-		writer.value(latitude);
+		if(null!=latitude)
+			writer.value(latitude);
+		else
+			writer.value(dLatitude.toString());
+			
 		writer.key("longitude");
-		writer.value(longitude);
+		if(null!=longitude)
+			writer.value(longitude);
+		else
+			writer.value(dLatitude.toString());
+	}
+	public Double distance(Location location)
+	{
+		if(null==getDoubleLatitude() || null == getDoubleLongitude() || 
+				null == location.getDoubleLatitude() || null== location.getDoubleLongitude())
+			return null;
+		
+		return distance(getDoubleLatitude(), getDoubleLongitude(), 
+				location.getDoubleLatitude(), location.getDoubleLongitude())*1000;
 	}
 
-	
+	protected double distance(double lat1, double lon1, double lat2, double lon2) {
+		  double theta = lon1 - lon2;
+		  double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+		  dist = Math.acos(dist);
+		  dist = rad2deg(dist);
+		  dist = dist * 60 * 1.1515;
+		  //if (unit == "K") {
+		    dist = dist * 1.609344;
+//		  } else if (unit == "N") {
+//		  	dist = dist * 0.8684;
+//		    }
+		  return (dist);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::  This function converts decimal degrees to radians             :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private double deg2rad(double deg) {
+		  return (deg * Math.PI / 180.0);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::  This function converts radians to decimal degrees             :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private double rad2deg(double rad) {
+		  return (rad * 180.0 / Math.PI);
+		}	
 }
--- trunk/WhereIsSchtief/src/org/schtief/whereisschtief/LocationManager.java
+++ trunk/WhereIsSchtief/src/org/schtief/whereisschtief/LocationManager.java
@@ -1,5 +1,6 @@
 package org.schtief.whereisschtief;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -28,11 +29,84 @@ public class LocationManager
 			}
 		}
 		//TODO name
-		jdoql.append(" ORDER BY time DESC");
+		jdoql.append(" ORDER BY time ASC");
 
-//		String jdoql = "SELECT FROM " + Location.class.getName()+" WHERE time >= "+startTimeStamp+" && time <= "+endTimeStamp+" ORDER BY time DESC";
-		System.out.println("HALLO"+jdoql);
-		return (List<Location>) pm.newQuery(jdoql).execute();
+//		String jdoql = "SELECT FROM " + Location.class.getName()+" WHERE time >= "+fromDate.getTimeInMillis()+" && time <= "+toDate.getTimeInMillis()+" ORDER BY time DESC";
+		System.out.println("HALLO "+jdoql);
+		return (List<Location>) pm.newQuery(jdoql.toString()).execute();
 	}
 
+	public static List<Location> getClusteredLocations(PersistenceManager pm, String name, Calendar fromDate, Calendar toDate, double thresholdRadius, int thresholdMinutes)
+	{
+		List<Location> locations	=	getLocations(pm, name, fromDate, toDate);
+		List<Location> result		=	new ArrayList<Location>();
+
+		Cluster cluster	=	null;
+		for (Location location : locations) 
+		{
+//			if(null==lastLocation)
+//			{
+//				lastLocation = location;
+//				continue;
+//			}
+			//wenn Cluster noch nicht da erstellen mit Location
+			if(null==cluster)
+			{
+				cluster	=	new Cluster(location);
+				continue;
+			}
+			//wenn abstand zum Cluster < thresholdRadius
+			if(cluster.distance(location)<=thresholdRadius)
+			{
+				System.out.println("Cluster Distance ok ADD : "+cluster.distance(location));
+				//zum cluster hinzufügen
+				cluster.add(location);
+				//continue
+			}
+			else //wenn abstand zum Cluster > thresholdRadius
+			{
+				System.out.println("Cluster Distance not ok : "+cluster.distance(location));
+
+				//calc LocationT-startT
+				//if dt > thresholdMinutes
+				if((location.getTime()-cluster.getTime())/(1000*60)>=thresholdMinutes)
+				{
+					System.out.println("Cluster sum time OK "+((location.getTime()-cluster.getTime())/(1000*60)));
+					//set last time
+					cluster.setLastTime(location.getTime());
+					//calc center add to List
+					result.add(cluster);
+				}
+				else //if dt < thresholdMinutes
+				{
+					System.out.println("Cluster sum time not OK "+((location.getTime()-cluster.getTime())/(1000*60)));
+
+					//add whole cluster Members to List
+					result.addAll(cluster.getMembers());
+				}
+				//new cluster add Location
+				cluster	=	new Cluster(location);
+				
+			}
+		}	
+		//cluster kann noch voll sein.
+		//calc LocationT-startT
+		//if dt > thresholdMinutes
+		if((locations.get(locations.size()-1).getTime()-cluster.getTime())/(1000*60)>=thresholdMinutes)
+		{
+			System.out.println("Last Cluster sum time OK "+((locations.get(locations.size()-1).getTime()-cluster.getTime())/(1000*60)));
+			//calc center add to List
+			result.add(cluster);
+		}
+		else //if dt < thresholdMinutes
+		{
+			System.out.println("Last Cluster sum time not OK "+((locations.get(locations.size()-1).getTime()-cluster.getTime())/(1000*60)));
+
+			//add whole cluster Members to List
+			result.addAll(cluster.getMembers());
+		}
+		return result;
+	}
+
+	
 }
